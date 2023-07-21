4f41fb8ca2e978da1994a5da1df5f6b48b2ac8c7
stark-x <92368824+unos01@users.noreply.github.com>
2023-07-21 22:55:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const slides=document.querySelectorAll(".slide");
+// console.log(slides);
+var counter=0;
+
+slides.forEach((slide,index)=>{
+    slide.style.left=`${index*100}%`;
+})
+
+const goNext=()=>{
+    counter++;
+    slideImage();
+}
+
+const goPrev=()=>{
+    counter--;
+    slideImage();
+}
+
+const slideImage=()=>{
+    slides.forEach(
+        (slide)=>{
+            slide.style.transform=`translateX(-${counter*100}%)`
+        }
+    )
+}
